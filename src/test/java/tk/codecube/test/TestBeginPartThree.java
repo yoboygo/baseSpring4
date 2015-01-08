5bb1b016a44479e96849ced4ec441a56a9f0a8db
Aimy <Aimy@Aimy>
2015-01-08 13:01:03 +0800

    add LocalThread

---
+++ src/test/java/tk/codecube/test/TestBeginPartThree.java
@@ -0,0 +1,16 @@
+/**
+ * TestBeginChapterSix.java
+ * 
+ * Aimy
+ * 下午10:45:20
+ */
+package tk.codecube.test;
+
+
+/**Cod Spring for part three
+ * @author Aimy
+ * 2014年10月13日 下午10:45:21
+ */
+public class TestBeginPartThree {
+	
+}
