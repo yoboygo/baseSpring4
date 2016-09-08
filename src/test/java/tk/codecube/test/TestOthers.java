d36ec772f42905540279632219761d7d536467dd
yoboygo <yoboygo@gmail.com>
2016-09-08 17:26:06 +0800

    添加关于Thread和XML验证的代码

--- src/test/java/tk/codecube/test/TestOthers.java
+++ src/test/java/tk/codecube/test/TestOthers.java
@@ -10,10 +10,24 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
 /**
  * 除Spring之外的各种小的Case
@@ -68,4 +82,60 @@ public class TestOthers {
 			printList(list);
 		}
 	}
+	
+	/**
+	 * ThreadStack：线程调用堆栈测试
+	 */
+	@Test
+	public void testThreadStack()
+	{
+		System.out.println("================开始===============");
+
+		printThreadStack();
+		
+		System.out.println("================结束===============");
+		
+	}
+	public void printThreadStack()
+	{
+		StackTraceElement[] stackTraceElement = Thread.currentThread().getStackTrace();
+		for(StackTraceElement e:stackTraceElement)
+		{
+			System.out.println(e.getClassName()+"	--->	"+e.getMethodName());
+		}
+	}
+	
+	/**
+	 * 测试Dom解析XML
+	 * @throws DocumentException
+	 * @throws SAXException 
+	 * @throws IOException 
+	 */
+	@Test
+	public void testXmlSchema() throws DocumentException, SAXException, IOException
+	{
+		String fileInPath = "config/taskConfig.xml";
+		String schemaPath = "config/taskConfig.xsd";
+		
+		URL fileBasePath = ClassLoader.getSystemResource("");
+		
+		InputStream isIn =  ClassLoader.getSystemResourceAsStream(fileInPath);
+		
+		SAXReader sr = new SAXReader();
+		Document doc = sr.read(isIn);
+		
+		Element root = doc.getRootElement();
+		System.out.println(root.asXML());
+		
+		SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema = sf.newSchema(new File(fileBasePath+schemaPath));
+		Validator validator = schema.newValidator();
+		try{
+			validator.validate(new StreamSource(new File(fileBasePath+fileInPath)));
+		}catch(SAXException e){
+			System.out.println("校验失败！");
+		}
+		
+	}
+	
 }
